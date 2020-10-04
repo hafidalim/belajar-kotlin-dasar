f4724c14945030ea68df40467c6b4aafa3bf1f85
hafidam <hafidd456@gmail.com>
2020-10-04 22:24:52 +0700

    variable

---
+++ src/Variable.kt
@@ -0,0 +1,15 @@
+/*
+Membuat constant
+ */
+const val APP = "Belajar Kotlin"
+const val VERSION = "1.0"
+
+fun main() {
+    val firstName = "Hafid"
+    val age = 19
+    val contohNul : String? = null
+    println(contohNul?.length)
+
+    println("$APP : version $VERSION")
+
+}
